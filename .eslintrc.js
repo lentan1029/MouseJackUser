3476a930136ce82e59d633eca8ea199f5fad5872
lentan <lentan1029@gmail.com>
2016-08-29 17:54:06 -0700

    Made basic user interface

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   parserOptions: {
     sourceType: "module",
     ecmaFeatures: {
+      'modules': true,
       'jsx': true
     }
   },
--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
+/*jshint esversion: 6 */
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
   }
+  handleClick(direction) {
+    $.ajax({
+      type: 'POST',
+      url: 'http://138.68.49.22:3000/user',
+      data: JSON.stringify({
+        x: 10,
+        y: 10
+      }),
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>MouseJackUser
+          </h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div onClick={this.handleClick.bind(this, 'left')} className="leftArrow">
+          left
+        </div>
+        <div onClick={this.handleClick.bind(this, 'right')} className="rightArrow">
+          right
+        </div>
+        <div onClick={this.handleClick.bind(this, 'up')} className="upArrow">
+          up
+        </div>
+        <div onClick={this.handleClick.bind(this, 'down')} className="downArrow">
+          down
+        </div>
       </div>
     );
   }
